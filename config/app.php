b8b6cb2fba4f5ff812b5df9b45e012c2ce3ffd52
tranhuuhai8 <tranhuuhaixtnd852003@gmail.com>
2025-06-08 10:51:56 +0700

    Update config app url home, clear cache view

--- config/app.php
+++ config/app.php
@@ -55,6 +55,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'url_home' => env('APP_URL_HOME', 'http://localhost:3024/auth/login'),
 
     /*
     |--------------------------------------------------------------------------
