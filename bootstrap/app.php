efbfd17df193c4378417cf3f96233fced9b37b46
tranhuuhai8 <tranhuuhaixtnd852003@gmail.com>
2025-08-06 21:39:50 +0700

    Feature seeder - migration mst

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\Cors;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -29,5 +30,20 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ValidationException $e, $request) {
+        $errors = $e->errors();
+
+        $firstError = $errors[array_key_first($errors)][0];
+        $errorCount = collect($errors)->flatten()->count() - 1;
+
+        $customMessage = $firstError;
+        if ($errorCount) {
+            $customMessage .= " và còn {$errorCount} lỗi khác";
+        }
+
+        return response()->json([
+            'message' => $customMessage,
+            'errors'  => $errors
+        ], $e->status);
+    });
     })->create();
